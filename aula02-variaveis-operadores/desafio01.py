d5a61c2ecec145a1d5a7c651f0895b565f262ffe
NicolasRodrigues07 <nicolas.a.rodrigues11@gmail.com>
2026-03-16 22:43:35 -0300

    aula02-variaveis

---
+++ aula02-variaveis-operadores/desafio01.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome")
+print(f"Bem-vindo {nome}")
+
+dia = int(input("digite apenas o dia do seu nascimento"))
+mes = int(input("digite apenas o mês do seu nascimento"))
+ano = int(input("digite apenas o ano do seu nascimento"))
+
+print(f'sua data de nascimento é {dia}/{mes}/{ano}')
+
+num1 = int(input("Digite o primeiro número para sua conta"))
+num2 = int(input("Digite o segundo número para sua conta"))
+print(f'A soma dos números {num1} e {num2} é {num1 + num2}')
